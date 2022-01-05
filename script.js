84170622b8b06bbb456a7d9fd9afc2a9acb405cb
Tyrik Williams <tyrik.w@yahoo.com>
2022-01-04 21:41:37 -0500

    p

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // TODO: Write Your JavaScript Code Here
+var students = ['j', 'd', 't', 'a'];
+
+console.log(students.length);
+
+for (var i = 0; i< students.length; i++) {
+    console.log('great to see you,' + students[i] + '!');
+}
